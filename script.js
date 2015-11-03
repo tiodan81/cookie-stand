d996e3c815b72bdc45b680d57496521a72a50317
Dan Schwartz <dpschwartz@gmail.com>
2015-11-02 21:17:33 -0600

    Create shop objects and functionality to generate models of hourly cookie sales.

---
+++ script.js
@@ -0,0 +1,58 @@
+function Shop(shopID, minCustHour, maxCustHour, avgCookiesCust) {
+  this.shopID = shopID;
+  this.minCustHour = minCustHour;
+  this.maxCustHour = maxCustHour;
+  this.avgCookiesCust = avgCookiesCust;
+  this.custPerHour = function () {
+    return (Math.floor(Math.random() * (this.maxCustHour - this.minCustHour)) + this.minCustHour);
+  };
+  this.hourlyTotals = []
+}
+
+var pikePlace = new Shop('pikePlace', 17, 88, 5.2);
+var seaTac = new Shop('seaTac', 6, 44, 1.2);
+var southcenter = new Shop('southcenter', 11, 38, 1.9);
+var bellevue = new Shop('bellevue', 20, 48, 3.3);
+var alki = new Shop('alki', 3, 24, 2.6);
+
+var allShops = [pikePlace, seaTac, southcenter, bellevue, alki];
+var times = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', 'total'];
+
+function cookiesPerHour(location) {
+  return Math.floor(location.custPerHour() * location.avgCookiesCust);
+}
+
+function hourlyModel(location) {
+  var hourlyArray = location.hourlyTotals;
+  var total = 0;
+
+  for (var i = 0; i < 8; i++) {
+    var cookies = cookiesPerHour(location);
+    hourlyArray.push(cookies);
+    total += cookies;
+  }
+
+  hourlyArray.push(total);
+  location.hourlyTotals = hourlyArray;
+}
+
+function hourlyReport (location) {
+  for (var j = 0; j < times.length; j++) {
+    var liNode = document.createElement('LI');
+    var hourlyNum = location.hourlyTotals[j];
+    var hourlyNumToString = hourlyNum.toString();
+    var text = times[j] + ': ' + hourlyNumToString + ' cookies.'
+    var liText = document.createTextNode(text);
+    liNode.appendChild(liText);
+    document.getElementById(location.shopID).appendChild(liNode);
+  }
+}
+
+function reportAllShops () {
+ for (var k = 0; k < allShops.length; k++) {
+    hourlyModel(allShops[k]);
+    hourlyReport(allShops[k]);
+  }
+}
+
+reportAllShops();
